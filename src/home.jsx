4dd75cf5bc5902a4cc3062d59da0e847ac5e9491
ShubhamVision <154418396+ShubhamVision@users.noreply.github.com>
2023-12-21 11:29:57 +0530

    shubham-billabadmash

--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import MainFooter from './mainFooter';
 import NavbarHome from './navbarHome';
+import MainFooter from './mainFooter';
 import { Link } from 'react-router-dom';
+import catImage from './cat.png';
 function home(props) {
     return (
-        <>
-         <NavbarHome />
+        <>       
+          <NavbarHome />
           <div>
             <h1>Welcome to Blogging app</h1>
+            <img src={catImage} alt="catImage" className="img-fluid w-100" style={{ height: "150px" }} />
             <Link to="/"></Link>
             <h3>Click below to begin</h3>
             <button><Link to='/Login'>Login</Link></button>
